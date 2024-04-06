e7a610d19e3b458a520b04976df71ddee11ff2b7
Ilya Sokolov <ilya2306@inbox.ru>
2024-04-06 22:20:42 +0300

    feat: Реализован функционал фильтра

--- script/script.js
+++ script/script.js
@@ -1,211 +1,194 @@
-document.addEventListener('DOMContentLoaded', () => {
-  new Lightpick({
-    field: document.getElementById('datepicker'),
-    singleDate: !1,
-    numberOfColumns: 2,
-    numberOfMonths: 2
-  })
+"use strict";
 
-  setCheckBoxState()
-  getItems()
-  setCalendarState()
-})
-
-const calendar = document.getElementById('calendar')
-
-function setCheckBoxState() {
-  // Получение необходимых элементов из DOM
-  const hiddenInputPlace = document.getElementById('hidden_input_place')
-  const searchPlaceInput = document.getElementById('search_place_input')
-  const hiddenInputPrice = document.getElementById('hidden_input_price')
-  const searchPriceInput = document.getElementById('search_price_input')
-  const hiddenInputDate = document.getElementById('hidden_input_date')
-  const calendar = document.getElementById('calendar')
-  const hiddenInputName = document.getElementById('hidden_input_name')
-  const searchInput = document.getElementById('search_input')
-
-  // Установка значений элементов, если они не равны null
-  if (hiddenInputPlace.value !== null) {
-    searchPlaceInput.value = hiddenInputPlace.value
-  }
-  if (hiddenInputPrice.value !== null) {
-    searchPriceInput.value = hiddenInputPrice.value
-  }
-  if (hiddenInputDate.value !== null) {
-    calendar.value = hiddenInputDate.value
-  }
-  if (hiddenInputName.value !== null) {
-    searchInput.value = hiddenInputName.value
-  }
-
-  // Получение чекбоксов из DOM
-  const allPlace = document.getElementById('allPlace')
-  const online = document.getElementById('online')
-  const isFree = document.getElementById('isFree')
-  const isNotFree = document.getElementById('isNotFree')
-
-  // Установка состояния чекбоксов на основе значений hidden input
-  if (hiddenInputPlace.value === allPlace.value) {
-    allPlace.checked = true
-  }
-  if (hiddenInputPlace.value === online.value) {
-    online.checked = true
-  }
-  if (hiddenInputPrice.value === isFree.value) {
-    isFree.checked = true
-  }
-  if (hiddenInputPrice.value === isNotFree.value) {
-    isNotFree.checked = true
-  }
-}
-
-function getItems() {
-  document.getElementById('search_input').addEventListener('click', event => {
+function addSearchClickListener() {
+  document.getElementById("search_input").addEventListener("click", event => {
     // Выбираем список элементов
-    const searchInputList = document.querySelector('.js-search_input_list')
-    const searchItems = searchInputList.querySelectorAll('.search_input-list_item')
+    const searchInputList = document.querySelector(".js-search_input_list");
+    const searchItems = searchInputList.querySelectorAll(".search_input-list_item");
+    const searchInput = document.getElementById("search_input");
 
-    // Отображаем первые 7 элементов, остальные скрываем
-    searchItems.forEach((item, index) => {
-      if (index < 7) {
-        item.style.display = ''
-      } else {
-        item.style.display = 'none'
-      }
-    })
-  })
+    if (searchInput.value) {
+      filterSearchResults();
+    } else {
+      searchItems.forEach((item, index) => {
+        if (index < 7) {
+          item.style.display = "";
+        } else {
+          item.style.display = "none";
+        }
+      });
+    }
+  });
 }
 
 function filterSearchResults() {
-  let searchInput = document.getElementById('search_input')
-  let searchText = searchInput.value.toUpperCase()
+  const searchInput = document.getElementById("search_input");
+  const searchText = searchInput.value.toUpperCase();
 
-  let searchList = document.getElementById('search_input_list')
-  let listItems = searchList.getElementsByTagName('li')
+  const searchList = document.getElementById("search_input_list");
+  const listItems = searchList.getElementsByTagName("li");
 
   for (let i = 0; i < listItems.length; i++) {
-    let link = listItems[i].getElementsByTagName('a')[0]
-    let linkText = link.innerHTML.toUpperCase()
+    const link = listItems[i].getElementsByTagName("a")[0];
+    const text = link.text?.toUpperCase();
 
-    if (linkText.indexOf(searchText) > -1) {
-      listItems[i].style.display = ''
+    if (text.indexOf(searchText) > -1) {
+      listItems[i].style.display = "";
     } else {
-      listItems[i].style.display = 'none'
+      listItems[i].style.display = "none";
     }
   }
-}
-
-function setCalendarState() {
-  // Обработчик клика по элементу calendar
-  calendar.addEventListener('click', e => {
-    e.target.classList.toggle('open-calendar')
-
-    // Показать/скрыть lightpick в зависимости от наличия класса open-calendar
-    if (e.target.classList.contains('open-calendar')) {
-      document.querySelector('.lightpick').classList.remove('is-hidden')
-    } else {
-      document.querySelector('.lightpick').classList.add('is-hidden')
-    }
-  })
 }
 
 // Функция обработки кликов на документе
 function handleDocumentClick(e) {
-  const searchPlaceInput = document.getElementById('search_place_input')
-  const searchPriceInput = document.getElementById('search_price_input')
-  const searchInputListPlace = document.getElementById('search_input_list_place')
-  const searchInputListPrice = document.getElementById('search_input_list_price')
-  const lightpick = document.querySelector('.lightpick')
+  const searchPlaceInput = document.getElementById("search_place_input");
+  const searchPriceInput = document.getElementById("search_price_input");
+  const searchInputListPlace = document.getElementById("search_input_list_place");
+  const searchInputListPrice = document.getElementById("search_input_list_price");
 
   if (e.target === searchPlaceInput) {
-    searchInputListPlace.classList.toggle('show')
-    searchInputListPrice.classList.remove('show')
+    searchInputListPlace.classList.toggle("show");
+    searchInputListPrice.classList.remove("show");
   } else if (e.target === searchPriceInput) {
-    searchInputListPrice.classList.toggle('show')
-    searchInputListPlace.classList.remove('show')
+    searchInputListPrice.classList.toggle("show");
+    searchInputListPlace.classList.remove("show");
   } else {
-    if (e.target.closest('.search_input_container-price_item') || e.target.closest('.search_input_container-place_item')) {
-      return
+    if (e.target.closest(".search_input_container-price_item") || e.target.closest(".search_input_container-place_item")) {
+      return;
     }
-    searchInputListPrice.classList.remove('show')
-    searchInputListPlace.classList.remove('show')
-  }
-
-  if (lightpick.classList.contains('is-hidden')) {
-    calendar.classList.remove('open-calendar')
+    searchInputListPrice.classList.remove("show");
+    searchInputListPlace.classList.remove("show");
   }
 }
 
-document.addEventListener('click', handleDocumentClick);
 
 // Функция обработки кликов на элементе search_place_input
 function handleSearchPlaceInputClick(e) {
-  const searchInputPlace = document.getElementById('search_input_place')
-  const allPlace = document.getElementById('allPlace')
-  const online = document.getElementById('online')
+  const inputPlace = document.getElementById("search_input_place");
+  const placeAll = document.getElementById("allPlace");
+  const placeOnline = document.getElementById("online");
 
-  allPlace.addEventListener('click', () => {
-    if (allPlace.checked) {
-      e.target.value = allPlace.value
-      online.checked = false
+  placeAll.addEventListener("click", () => {
+    if (placeAll.checked) {
+      e.target.value = placeAll.value;
+      placeOnline.checked = false;
     } else {
-      e.target.value = ''
+      e.target.value = "";
     }
-  })
+  }, {once: true});
 
-  online.addEventListener('click', () => {
-    if (online.checked) {
-      e.target.value = online.value
-      allPlace.checked = false
+  placeOnline.addEventListener("click", () => {
+    if (placeOnline.checked) {
+      e.target.value = placeOnline.value;
+      placeAll.checked = false;
     } else {
-      e.target.value = ''
+      e.target.value = "";
     }
-  })
+  }, {once: true});
 
-  searchInputPlace.addEventListener('click', () => {
-    allPlace.checked = false
-    online.checked = false
-    e.target.value = searchInputPlace.value
-  })
+  inputPlace.addEventListener("click", () => {
+    placeAll.checked = false;
+    placeOnline.checked = false;
+    e.target.value = inputPlace.value;
+  }, {once: true});
 
-  searchInputPlace.addEventListener('change', () => {
-    allPlace.checked = false
-    e.target.value = searchInputPlace.value
-  })
+  inputPlace.addEventListener("change", () => {
+    placeAll.checked = false;
+    e.target.value = inputPlace.value;
+  }, {once: true});
 
-  if (allPlace.checked || online.checked) {
-    e.target.value = ''
-  }
-
-  if (online.checked) {
-    allPlace.checked = false
-  }
+  (placeAll.checked || placeOnline.checked) && (inputPlace.value = "");
+  placeOnline.checked && (placeAll.checked = false);
 }
-
-document.getElementById('search_place_input').addEventListener('click', handleSearchPlaceInputClick);
 
 // Функция обработки кликов на элементе search_price_input
 function handleSearchPriceInputClick(event) {
-  const isFree = document.getElementById('isFree')
-  const isNotFree = document.getElementById('isNotFree')
+  const isFree = document.getElementById("isFree");
+  const isNotFree = document.getElementById("isNotFree");
 
   function handleCheckboxClick(checkbox1, checkbox2) {
-    checkbox1.addEventListener('click', () => {
+    checkbox1.addEventListener("click", () => {
       if (checkbox1.checked) {
-        event.target.value = checkbox1.value
-        checkbox2.checked = false
+        event.target.value = checkbox1.value;
+        checkbox2.checked = false;
       } else {
-        event.target.value = ''
+        event.target.value = "";
       }
-    })
+    }, {once: true});
   }
 
-  handleCheckboxClick(isNotFree, isFree)
-  handleCheckboxClick(isFree, isNotFree)
+  handleCheckboxClick(isNotFree, isFree);
+  handleCheckboxClick(isFree, isNotFree);
 
   if (!isFree.checked && !isNotFree.checked) {
-    event.target.value = ''
+    event.target.value = "";
   }
 }
 
-document.getElementById('search_price_input').addEventListener('click', handleSearchPriceInputClick);
+document.addEventListener("DOMContentLoaded", () => {
+  new Lightpick({
+    field: document.getElementById("datepicker"), singleDate: false, numberOfColumns: 2, numberOfMonths: 2, autoclose: true, hideOnBodyClick: true
+  });
+
+  addSearchClickListener();
+  document.getElementById("search_price_input").addEventListener("click", handleSearchPriceInputClick);
+  document.getElementById("search_place_input").addEventListener("click", handleSearchPlaceInputClick);
+  document.addEventListener("click", handleDocumentClick);
+
+  const locationUrl = new URL(location.href);
+  const searchParams = locationUrl.searchParams;
+
+  if (searchParams) {
+    const place = document.getElementById("search_place_input");
+    const price = document.getElementById("search_price_input");
+    const date = document.getElementById("datepicker");
+    const searchInput = document.getElementById("search_input");
+
+    if (searchParams.has("name")) {
+      const searchName = searchParams.get("name");
+      searchInput.value = searchName || "";
+    }
+
+    if (searchParams.has("date")) {
+      const searchDate = searchParams.get("date");
+      date.value = searchDate || "";
+    }
+
+    if (searchParams.has("place")) {
+      const searchPlace = searchParams.get("place");
+
+      if (searchPlace) {
+        const placeAll = document.getElementById("allPlace");
+        const placeOnline = document.getElementById("online");
+
+        if (searchPlace === placeAll.value) {
+          placeAll.checked = true;
+          place.value = placeAll.value;
+        } else if (searchPlace === placeOnline.value) {
+          placeOnline.checked = true;
+          place.value = placeOnline.value;
+        } else {
+          place.value = searchPlace;
+          document.querySelector("#search_input_place").value = searchPlace;
+        }
+      }
+    }
+
+    if (searchParams.has("price")) {
+      const searchPrice = searchParams.get("price");
+      if (searchPrice) {
+        const coastFree = document.getElementById("isFree");
+        const coastNotFree = document.getElementById("isNotFree");
+
+        if (searchPrice === coastFree.value) {
+          coastFree.checked = true;
+          price.value = coastFree.value;
+        } else if (searchPrice === coastNotFree.value) {
+          coastNotFree.checked = true;
+          price.value = coastNotFree.value;
+        }
+      }
+    }
+  }
+});
